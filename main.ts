0bc826027246be90676e20da86cf6c38d2a34c71
MrZebarth <kris.zebarth@ocsb.ca>
2020-01-03 21:56:42 -0500

    Cursor Working. Moved the images array into a separate file

--- main.ts
+++ main.ts
@@ -2,53 +2,78 @@ namespace SpriteKind {
     export const UI = SpriteKind.create()
 }
 function InitKeys () {
-    keypadList = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27]
     for (let index = 0; index <= keypadList.length - 1; index++) {
         mySprite = sprites.create(keypadList[index], SpriteKind.UI)
         mySprite.left = index % 7 * 23
         mySprite.top = Math.floor(index / 7) * 31
     }
-    mySprite = sprites.create(img`
-        f f f f f . . . . . . . . . . f f f f f
-        f . . . . . . . . . . . . . . . . . . f
-        f . . . . . . . . . . . . . . . . . . f
-        f . . . . . . . . . . . . . . . . . . f
-        f . . . . . . . . . . . . . . . . . . f
-        f . . . . . . . . . . . . . . . . . . f
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        f . . . . . . . . . . . . . . . . . . f
-        f . . . . . . . . . . . . . . . . . . f
-        f . . . . . . . . . . . . . . . . . . f
-        f . . . . . . . . . . . . . . . . . . f
-        f f f f f . . . . . . . . . . f f f f f
-    `, SpriteKind.UI)
-    mySprite.left = 0
-    mySprite.top = 0
-    mySprite2 = 0
+    cursorSprite = sprites.create(img`
+8 8 8 8 8 . . . . . . . . . . 8 8 8 8 8 
+8 8 8 8 8 . . . . . . . . . . 8 8 8 8 8 
+8 8 . . . . . . . . . . . . . . . . 8 8 
+8 8 . . . . . . . . . . . . . . . . 8 8 
+8 8 . . . . . . . . . . . . . . . . 8 8 
+8 8 . . . . . . . . . . . . . . . . 8 8 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+8 8 . . . . . . . . . . . . . . . . 8 8 
+8 8 . . . . . . . . . . . . . . . . 8 8 
+8 8 . . . . . . . . . . . . . . . . 8 8 
+8 8 . . . . . . . . . . . . . . . . 8 8 
+8 8 . . . . . . . . . . . . . . . . 8 8 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+8 8 . . . . . . . . . . . . . . . . 8 8 
+8 8 . . . . . . . . . . . . . . . . 8 8 
+8 8 . . . . . . . . . . . . . . . . 8 8 
+8 8 8 8 8 . . . . . . . . . . 8 8 8 8 8 
+8 8 8 8 8 . . . . . . . . . . 8 8 8 8 8 
+`, SpriteKind.UI)
+    cursorSprite.left = 0
+    cursorSprite.top = 0
+    curX = 0
+    curY = 0
 }
+function UpdateCursor () {
+    cursorSprite.left = curX * 23
+    cursorSprite.top = curY * 31
+}
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    curX = Math.max(0, curX - 1)
+    UpdateCursor()
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    curY = Math.min(3, curY + 1)
+    UpdateCursor()
+})
 sprites.onCreated(SpriteKind.UI, function (sprite) {
     sprite.setFlag(SpriteFlag.Ghost, true)
 })
-let mySprite2 = 0
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (curY < 3) {
+        curX = Math.min(6, curX + 1)
+    } else {
+        curX = Math.min(5, curX + 1)
+    }
+    UpdateCursor()
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    curY = Math.max(0, curY - 1)
+    UpdateCursor()
+})
+let curY = 0
+let curX = 0
+let cursorSprite: Sprite = null
 let mySprite: Sprite = null
-let keypadList: Image[] = []
 scene.setBackgroundImage(bgImage)
 InitKeys()
--- symbols.ts
+++ symbols.ts
@@ -1017,3 +1017,5 @@ bgImage.drawLine(90, 0, 90, 120, 15)
 bgImage.drawLine(113, 0, 113, 120, 15)
 bgImage.drawLine(136, 0, 136, 120, 15)
 
+let keypadList: Image[] = []
+keypadList = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27]
